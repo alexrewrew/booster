4595bf335653b74d5c2bef429b1483af4ce898c4
alexrewrew <alexrewrew@live.com>
2016-10-25 15:37:30 +0300

    Almost

--- assets/js/plugins.js
+++ assets/js/plugins.js
@@ -1,14 +1,26 @@
 $(document).ready(function () {
     /* WOW.JS */
     new WOW().init();
-    
     /* OWL CAROUSEL */
     $('#customers').owlCarousel({
         loop: true
         , margin: 10
         , nav: true
-        , items: 5,
-        navText: ["<img src='assets/img/left_arrow.png'>","<img src='assets/img/right_arrow.png'>"]
+        , items: 5
+        , navText: ["<img src='assets/img/left_arrow.png'>", "<img src='assets/img/right_arrow.png'>"]
+        , responsive: {
+            // breakpoint from 0 up
+            0: {
+                items: 2
+                , nav: false
+            }, // breakpoint from 768 up
+            768: {
+                items: 3
+            }
+            , 1024: {
+                items: 4
+            }
+        }
     });
     /* SLICK */
     $('.your-class').slick({});
