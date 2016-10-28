81b97e90cd5ed6f59abec83b7badcf1b9274beda
alexrewrew <alexrewrew@live.com>
2016-10-28 13:47:13 +0300

    Boos

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -52,6 +52,23 @@ $(document).ready(function () {
     $(function () {
         $('[data-scroll-speed]').moveIt();
     });
+
+    $('.minus').click(function () {
+            var $input = $(this).parent().find('input');
+            var count = parseInt($input.val()) - 1;
+            count = count < 1 ? 1 : count;
+            $input.val(count);
+            $input.change();
+            return false;
+        });
+        $('.plus').click(function () {
+            var $input = $(this).parent().find('input');
+            $input.val(parseInt($input.val()) + 1);
+            $input.change();
+            return false;
+        });
+
+
 });
 var isMobile = {
     Android: function () {
